e071758dec237d936cb5784e11ea96041d3126fc
Itshivamgithub <shivamrajbhar883@gmail.com>
2026-05-30 14:24:46 +0530

    Update Contact form with user EmailJS credentials

--- NewPortfolio/src/components/Contact.jsx
+++ NewPortfolio/src/components/Contact.jsx
@@ -33,16 +33,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_APP_EMAILJS_RECEIVERID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATEID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Shivam",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "shivamsde02@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_APP_EMAILJS_USERID
       )
       .then(
         () => {
